30cec26e95b37f8234de0199580d5dd5518976a7
SmithVyne <smithnkereuwem2@gmail.com>
2020-10-06 01:45:10 +0300

    Added todo list accordion

--- src/displayProject.js
+++ src/displayProject.js
@@ -1,54 +1,73 @@
+import '../css/styles.css'
 
-const loopTodos = (todos, index) => {
-  let tableBody = document.querySelector(`#todos${index}`);
+const theTodos = (todosAccord, todo, todoIndex, projIndex) => {
+  todosAccord.innerHTML += `
+  <div class="card">
+    <div class="card-header">
+      <h2 class="mb-0">
+        <div class="btn btn-link btn-block text-left todoHeader" type="button" data-toggle="collapse" data-target="#todo${todoIndex}">
+          <div>${todo.title}</div>
+          <div>${todo.dueDate}</div>
+        </div>
+      </h2>
+    </div>
+
+    <div id="todo${todoIndex}" class="collapse" data-parent="#todos${projIndex}">
+      <div class="card-body">
+        <p>Description: ${todo.description}</p>
+        <p>Priority: ${todo.priority}</p>
+        <button class="btn btn-warning">Edit</button>
+        <button class="btn btn-danger">Delete</button>
+      </div>
+    </div>
+  </div>
+  `
+};
+
+const loopTodos = (todos, projIndex) => {
+  let todosAccord = document.querySelector(`#todos${projIndex}`);
 
   const sortedTodo = todos.sort((a, b) => {
      return b.priority - a.priority
   })
 
-  for (let todo of sortedTodo) {
-    tableBody.innerHTML += `
-    <tr>
-      <td class='mx-4'>${todo.title}</td>
-      <td class='mx-4'>${todo.description}</td>
-      <td class='mx-4'>${todo.dueDate}</td>
-      <td class='mx-4'>${todo.priority}</td>
-    </tr>` 
-  }
+  sortedTodo.forEach((todo, todoIndex) => {
+    theTodos(todosAccord, todo, todoIndex, projIndex);
+  });
 };
 
-const projectCard = (accordion, project, index) => {
+const projectCard = (accordion, project, projIndex) => {
   let todos = project.list;
 
   accordion.innerHTML += `
     <div class="card">
       <div class="card-header" id="headingOne">
         <h2 class="mb-0">
-          <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse${index}" aria-expanded="true" aria-controls="collapseOne">
-            ${project.name}
+          <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse${projIndex}">
+            ${project.name} 
           </button>
         </h2>
       </div>
 
-      <div id="collapse${index}" class="collapse ${index == 0 ? 'show' : ''}" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div id="collapse${projIndex}" class="collapse ${projIndex == 0 ? 'show' : ''}" data-parent="#accordionExample">
         <div class="card-body">
-          <table class="table table-dark">
-            <thead>
-              <tr>
-                <th scope="col">Title</th>
-                <th scope="col">Description</th>
-                <th scope="col">Due date</th>
-                <th scope="col">Priority</th>
-              </tr>
-            </thead>
-            <tbody id="todos${index}"></tbody>
-          </table>
+
+          <!--Todo Starts here -->
+          <div class="accordion ml-auto mr-auto" id="todos${projIndex}" style="width: 90%;">
+            
+          </div>
+            
+        </div>
+        <div class="text-center mb-3">
+          <button class="btn btn-primary">Add Todo</button>
         </div>
       </div>
-    </div>`
+    </div>
+    `
 
-  loopTodos(todos, index);
+  loopTodos(todos, projIndex);
 };
+
 
 const displayProjects = (projects) => {
   const accordion = document.querySelector('#accordionExample');
